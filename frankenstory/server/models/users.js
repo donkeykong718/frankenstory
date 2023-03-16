e4438bca472a23efbd37f568cc7a5b9c13cb8555
Ahmed Rahi <54910341+arahi7860@users.noreply.github.com>
2023-03-16 15:18:50 -0400

    setting up the models

---
+++ frankenstory/server/models/users.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  drawings: [{ type: mongoose.Schema.Types.ObjectId, ref: "Drawing" }],
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
