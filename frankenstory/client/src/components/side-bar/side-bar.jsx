554fbaecce215f74ca366e56bda79aec56cf10be
richardsaudek <richard.saudek@gmail.com>
2023-03-17 14:46:28 -0400

    side nav bar scroll

--- frankenstory/client/src/components/side-bar/side-bar.jsx
+++ frankenstory/client/src/components/side-bar/side-bar.jsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect, useRef } from 'react';
-import './side-bar.css'
+import './side-bar.css';
+import CompletionCircle_1 from '../side-bar/side-bar assets/CompletionCircle_1.svg'
+import CompletionCircle_2 from '../side-bar/side-bar assets/CompletionCircle_2.svg'
+import CompletionCircle_3 from '../side-bar/side-bar assets/CompletionCircle_3.svg'
+import CompletionCircle_4 from '../side-bar/side-bar assets/CompletionCircle_4.svg'
+import CompletionCircle_5 from '../side-bar/side-bar assets/CompletionCircle_5.svg'
+import CompletionCircle_6 from '../side-bar/side-bar assets/CompletionCircle_6.svg'
+import CompletionCircle_7 from '../side-bar/side-bar assets/CompletionCircle_7.svg'
 
 function Sidebar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -23,9 +30,14 @@ function Sidebar() {
       <button onClick={toggleSidebar}>Open Sidebar</button>
     <div className={`sidebar ${isOpen ? 'open' : ''}`} ref={sidebarRef}>
       <ul>
-        <li>Link 1</li>
-        <li>Link 2</li>
-          <li>Link 3</li>
+          <li><img src={CompletionCircle_1} className="completionCircle"/></li>
+          <li><img src={CompletionCircle_2} className="completionCircle"/></li>
+          <li><img src={CompletionCircle_3} className="completionCircle"/></li>
+          <li><img src={CompletionCircle_4} className="completionCircle"/></li>
+          <li><img src={CompletionCircle_5} className="completionCircle"/></li>
+          <li><img src={CompletionCircle_6} className="completionCircle"/></li>
+          <li><img src={CompletionCircle_7} className="completionCircle"/></li>
+          
           <li>      <button onClick={toggleSidebar}>Close Sidebar</button></li>
       </ul>
     </div>
