1041bd20f5b7b60ce5ce81c8751daafc3de3f97a
William Chen <William565973@gmail.com>
2023-03-20 17:03:34 -0400

    in the works

--- frankenstory/client/src/App.jsx
+++ frankenstory/client/src/App.jsx
@@ -1,14 +1,9 @@
 import React, { useState } from 'react';
 import Sidebar from './components/side-bar/side-bar';
 import Header from './components/Header/Header';
-
 import Gallery from './components/TestFunctions/Gallery';
 import TestFunctions from './components/TestFunctions/TestFunctions';
-// import * as backendFunctions from './services/stories';
-// import '.side-bar'
-import DrawingBoard from './components/canvas-component/drawingBoard';
-// import Parent from './components/TestFunctions/Parent'
-
+import DrawingBoard from './components/SketchCanvas/drawingBoard';
 export const StoryContext = React.createContext();
 export const GalleryContext = React.createContext();
 
@@ -16,17 +11,6 @@ function App() {
 
   const [current, setCurrent] = useState({});
   const [featured, setFeatured] = useState({});
-
-  // async function getActiveStories() {
-  //   const activeStories = await backendFunctions.getStories();
-  //   console.log(typeof activeStories);
-  //   // const storyArray = Array.from(activeStories);
-  //   return activeStories;
-  // }
-  // const activeStories = getActiveStories();
-  // console.log(storyArray);
-  // console.log(typeof storyArray);
-
   return (
     <>
       <StoryContext.Provider value={{ current, setCurrent }}>
@@ -34,7 +18,7 @@ function App() {
         <Sidebar />
         <GalleryContext.Provider value={{ featured, setFeatured }}>
           <TestFunctions current={current} featured={featured} />
-                <DrawingBoard />
+          <DrawingBoard />
         </GalleryContext.Provider>
       </StoryContext.Provider>
     </>
